34ad9044c966b441df2a43fe13eaf556fdb5f52c
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2022-05-04 19:14:44 +0200

    added snappy for pdfs

--- src/Controller/adminController.php
+++ src/Controller/adminController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class adminController extends AbstractController
 {
     #[Route("/admin")]
-    public function adminPage()
+    public function adminPage(): Response
     {
+
     }
 }
